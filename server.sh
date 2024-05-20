9a121fa7abc33cbc67c114d0faba00172ea2104a
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-05-20 14:03:00 -0300

    Create server.sh

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -d --rm -p 8000:8000 -p 8001:8001 -p 8002:8002 --gpus all -v ./models:/models --name triton-server nvcr.io/nvidia/tritonserver:21.10-py3 tritonserver --model-repository=/models
+docker run -it --rm -p 8000:8000 -p 8001:8001 -p 8002:8002 -v ./models:/models --name triton-server triton-inference-server
